6f4fa234dd9ffffb2be3d8218b8794fd3c0f3860
Rishabh-Baranwal <rishabh.baranwal@avinetworks.com>
2017-08-17 14:43:20 +0530

    Updated docstring for the methods

--- python/avi/migrationtools/f5_converter/conversion_util.py
+++ python/avi/migrationtools/f5_converter/conversion_util.py
@@ -1647,7 +1647,13 @@ class F5Util(MigrationUtil):
         return vrf_ref
 
     def net_to_static_route(self, f5_config, avi_config):
-
+        """
+        This method converts the net route to static routes and updates the
+        VrfContext objects
+        :param f5_config: parsed f5 config
+        :param avi_config: converted config in avi
+        :return:
+        """
         net_config = f5_config.get('route', {})
         avi_vrf = avi_config["VrfContext"]
         # Convert net static route to vrf static route
@@ -1665,6 +1671,14 @@ class F5Util(MigrationUtil):
                             obj['static_routes'] = [static_route]
 
     def update_monitor_ssl_ref(self, avi_dict, merge_obj_dict, sysdict):
+        """
+        This method updates the first ssl profile reference from merge
+        perspective in monitors, which get attached at the time of creation
+        :param avi_dict: avi configuration dict
+        :param merge_obj_dict: dict having merge objects
+        :param sysdict: system object dicts
+        :return:
+        """
         for obj in avi_dict['HealthMonitor']:
             obj_ref = obj.get('https_monitor', {}).get('ssl_attributes',
                                                        {}).get(
@@ -1681,6 +1695,14 @@ class F5Util(MigrationUtil):
                         self.get_object_ref(updated_name, type_cons, tenant)
 
     def update_app_profile(self, aviconfig, sys_dict, tenant):
+        """
+        This method updates the application profile to http when there are
+        multiple services to a L4 app VS in which one of them is ssl enabled
+        :param aviconfig: avi config dict
+        :param sys_dict: system config dict
+        :param tenant: tenant to be used for reference
+        :return:
+        """
         for vs_obj in aviconfig['VirtualService']:
             if len(vs_obj['services']) > 1:
                 app_profile = self.get_name(vs_obj['application_profile_ref'])
