f1f7a5db3b3f6c30ea15f3a55ed7fff89df4d825
Rishabh-Baranwal <rishabh.baranwal@avinetworks.com>
2017-08-22 10:58:05 +0530

    Adding some policy request rule match

--- python/avi/migrationtools/f5_converter/policy_converter.py
+++ python/avi/migrationtools/f5_converter/policy_converter.py
@@ -1,10 +1,15 @@
 import logging
 import avi.migrationtools.f5_converter.converter_constants as final
+from avi.migrationtools.f5_converter.conversion_util.py import F5Util
 
 LOG = logging.getLogger(__name__)
 
+# Creating f5 object for util library.
+conv_utils = F5Util()
+
 parameters_dict = {'starts-with': 'BEGINS_WITH', 'equals': 'EQUALS',
-                   'contains': 'CONTAINS', 'ends-with': 'ENDS_WITH'}
+                   'contains': 'CONTAINS', 'ends-with': 'ENDS_WITH',
+                   'not': 'DOES_NOT_'}
 
 
 class PolicyConfigConv(object):
@@ -15,15 +20,17 @@ class PolicyConfigConv(object):
         # Get the policy config from converted parsing
         policy_config = f5_config.get("policy", {})
         print policy_config
+        avi_config['HTTPPolicySet'] = []
         for each_policy in policy_config:
             httppolicy = dict()
             httppolicy['name'] = each_policy
-            httppolicy['tenant_ref'] = '/api/tenant/admin'
-            httppolicy['http_request_policy'] = dict()
-            httppolicy['http_request_policy']['rules'] = []
-            self.create_rules(policy_config[each_policy], httppolicy)
+            httppolicy['tenant_ref'] = conv_utils.get_object_ref(tenant,
+                                                                 'tenant')
+            self.create_rules(policy_config[each_policy], httppolicy, tenant,
+                              avi_config)
+            avi_config['HTTPPolicySet'].append(httppolicy)
 
-    def create_rules(self, config, httppolicy):
+    def create_rules(self, config, httppolicy, tenant, avi_config):
         """
         :param config:
         :param httppolicy:
@@ -31,26 +38,127 @@ class PolicyConfigConv(object):
         """
         if 'rules' in config:
             for index, each_rule in enumerate(config['rules']):
-                if 'conditions' or 'actions' in config['rules'][each_rule]:
-                    global_dict = {'name': each_rule, 'enable': True, 'index': index +1}
-                if 'conditions' in config['rules'][each_rule]:
+                if 'conditions' and 'actions' in config['rules'][each_rule]:
+                    global_dict = {'name': each_rule, 'enable': True,
+                                   'index': index +1}
                     match_rule = config['rules'][each_rule]['conditions']
-                    self.create_match_rule(match_rule, httppolicy, global_dict)
-                if 'actions' in config['rules'][each_rule]:
-                    match_rule = config['rules'][each_rule]['actions']
-                    self.create_action_rule(match_rule, httppolicy, global_dict)
-                httppolicy['http_request_policy']['rules'].append(global_dict)
+                    pol_type = self.create_match_rule(match_rule, httppolicy,
+                                           global_dict, avi_config, tenant)
+                    action_rule = config['rules'][each_rule]['actions']
+                    self.create_action_rule(action_rule, httppolicy,
+                                            global_dict)
+                    httppolicy['http_' + pol_type + '_policy'] = dict()
+                    httppolicy['http_' + pol_type + '_policy']['rules'] = []
+                    httppolicy['http_' + pol_type + '_policy']['rules'].append(
+                                                                    global_dict)
 
-    def create_match_rule(self, match_dict, httppolicy, global_dict):
+    def create_match_rule(self, match_dict, httppolicy, global_dict,
+                          avi_config, tenant):
         """
         :param match_dict:
         :return:
         """
+        pol_type = None
         for each_index in match_dict:
-            result = match_dict[each_index].keys()
-            match_policy = dict()
+            result = match_dict[each_index]
+            pol_type = 'response' if 'response' in result else 'request'
+            match = None
+            path_query = {
+                "match_str": [],
+                "match_criteria": '',
+                'match_case': 'INSENSITIVE'
+            }
+
+            path_regex = {
+                "match_case": 'INSENSITIVE',
+                "string_group_refs": [],
+                "match_criteria": ''
+            }
+
+
+            host_header = {
+                "match_case": 'INSENSITIVE',
+                "value": [],
+                "match_criteria": ''
+            }
+
+            if 'geoip' in result:
+                if 'country-code' in result:
+                    client_ip = {
+                        'group_refs': [],
+                        'match_criteria': 'IS_NOT_IN' if 'not' in result
+                                            else 'IS_IN'
+                    }
+                    match = {'client_ip': client_ip}
+                    match['client_ip']['group_refs'].append(
+                                             '/api/ipaddrgroup/?name=Internal')
+                        #conv_utils.get_object_ref('Internal', 'ipaddrgroup',
+                                                  #tenant=tenant))
+                    ip_addr_group = {'name': 'Internal',
+                                     'tenant_ref': conv_utils.get_object_ref(
+                                         tenant, 'tenant'),
+                                     'country_code': result['values'].keys()
+                                     }
+                    if 'IpAddrGroup' not in avi_config:
+                        avi_config['IpAddrGroup'] = []
+                    avi_config['IpAddrGroup'].append(ip_addr_group)
+            elif 'http-cookie' in result:
+                cookie = {
+                    "match_case": 'INSENSITIVE',
+                    "name": result['name'],
+                    "value": result['values'].keys()[0],
+                    "match_criteria": ''
+                }
+
+                match_criteria = [key for key in result if key in
+                                  parameters_dict]
+                if len(match_criteria) > 1:
+                    cookie['match_criteria'] = 'HDR_' + parameters_dict[
+                        match_criteria[0]] + (parameters_dict[match_criteria[
+                        1]].replace('S',''))
+                elif len(match_criteria):
+                    if 'not' in match_criteria:
+                        cookie['match_criteria'] = 'HDR_' + parameters_dict[
+                                                   match_criteria[0]] + 'EQUAL'
+                    else:
+                        cookie['match_criteria'] = 'HDR_' + parameters_dict[
+                                                              match_criteria[0]]
+                else:
+                    cookie['match_criteria'] = 'HDR_EQUALS'
+                match = {'cookie': cookie}
+            elif 'http-header' in result:
+                header = {
+                    "match_case": 'INSENSITIVE',
+                    "hdr": result['name'],
+                    "value": result['values'].keys(),
+                    "match_criteria": ''
+                }
+                match_criteria = [key for key in result if key in
+                                  parameters_dict]
+                if len(match_criteria) > 1:
+                    header['match_criteria'] = 'HDR_' + parameters_dict[
+                        match_criteria[0]] + (parameters_dict[match_criteria[
+                        1]].replace('S', ''))
+                elif len(match_criteria):
+                    if 'not' in match_criteria:
+                        header['match_criteria'] = 'HDR_' + parameters_dict[
+                            match_criteria[0]] + 'EQUAL'
+                    else:
+                        header['match_criteria'] = 'HDR_' + parameters_dict[
+                            match_criteria[0]]
+                else:
+                    header['match_criteria'] = 'HDR_EQUALS'
+                match = {"hdrs": [header]}
+
+
+
+
+
+
+
             if 'path' and 'http-uri' in result:
-                match_criteria = [key for key in result if key in parameters_dict.keys()]
+                match_criteria = [key for key in result if key in
+                                  parameters_dict.keys()]
                 if match_criteria:
                     match_criteria = parameters_dict[str(match_criteria[0])]
                 match_policy = \
@@ -65,7 +173,9 @@ class PolicyConfigConv(object):
             if 'values' in match_dict[each_index].keys():
                 match_str = match_dict[each_index]['values'].keys()
                 match_policy['match']['path']['match_str'] = match_str
-                global_dict.update(match_policy)
+            if match:
+                global_dict.update({'match': match})
+        return pol_type
 
     def create_action_rule(self, action_dict, httppolicy, global_dict):
         """
